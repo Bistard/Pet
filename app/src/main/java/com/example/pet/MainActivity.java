62c4cfcf22cf873f9b3affe04fc8035ccffeefbf
Bistard <1015813038@qq.com>
2021-02-17 21:55:21 +0800

    added a button

--- app/src/main/java/com/example/pet/MainActivity.java
+++ app/src/main/java/com/example/pet/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.pet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.set_up1);
     }
+
+    // setUp page button click method
+    public void buttonClick (View view) {
+        TextView setupText = findViewById(R.id.setUpText1);
+        EditText petName = findViewById(R.id.setUpEnterBox1);
+        setupText.setText(petName.getText().toString());
+    }
+
 }
