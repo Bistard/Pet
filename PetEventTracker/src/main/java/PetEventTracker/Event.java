7a48689132d37ea02cc3881cf7daf224574ca26a
Liam Xue <liamxue2002@gmail.com>
2021-02-19 00:38:40 -0600

    Initial upload of this branch

---
+++ PetEventTracker/src/main/java/PetEventTracker/Event.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PetEventTracker;
+
+/**
+ *
+ * @author 30714
+ */
+abstract class Event {
+    
+}
+
+class Goal extends Event {
+    
+}
+
+class Task extends Event {
+    
+}
