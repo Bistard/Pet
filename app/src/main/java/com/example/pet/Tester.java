59d867f9a1eafbbae59f30154c7683f99df02953
Liam Xue <liamxue2002@gmail.com>
2021-03-06 11:54:11 -0600

    Update Tester.java

--- app/src/main/java/com/example/pet/Tester.java
+++ app/src/main/java/com/example/pet/Tester.java
@@ -15,14 +15,15 @@ public class Tester {
 
         user.isFirstTime = false;
         user.longTermGoal = "Finish this app";
-        user.longTermGoalEnd = 20210331;  // YYYYMMDD
+        user.longTermGoalEnd = 20220331;  // YYYYMMDD
 
-        Goal goal1 = user.addGoal("Goal 1", "",2021,02,20,2021,02,22,0);
-        user.addTask("Task 1","This is for goal 1", 2021,02,21, "Once", goal1);
-        user.addTask("Task 2","This is also for goal 1", 2021,02,22, "Weekly", goal1);
+        Goal goal1 = user.addGoal("Goal 1", "",2021,2,20,2021,2,22,0);
+        user.addTask("Task 1","This is for goal 1", 2021,2,21, "Once", goal1);
+        user.addTask("Task 2","This is also for goal 1", 2021,2,22, "Weekly", goal1);
 
-        int goal2ID = user.addGoal("Goal 2","",2021,02,20,2021,03,29,1).ID;
-        user.addTask("Task 3","For goal 2",2021,02,20,2021,03,20,"Bi-weekly",Goal.getGoalByID(goal2ID));  // none of the recurring rule is implemented yet
+        Goal goal2 = user.addGoal("Goal 2","",2020,2,20,2021,3,29,1);
+        user.addTask("Task","",2020,2,20,2021,3,20,"Monthly",goal2);  // none of the recurring rule is implemented yet
+        user.addTask("Task 3","For goal 2",2021,2,20,2021,3,20,"Bi-weekly",goal2);  // none of the recurring rule is implemented yet
 
         return user;
     }
