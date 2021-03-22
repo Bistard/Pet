e446146ddd2dea70ac24bbe83cab2d3837221ceb
Liam Xue <liamxue2002@gmail.com>
2021-03-21 20:32:48 -0600

    widget pet update

--- app/src/main/java/com/example/pet/PetWidget.java
+++ app/src/main/java/com/example/pet/PetWidget.java
@@ -1,8 +1,10 @@
 package com.example.pet;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
@@ -52,6 +54,19 @@ public class PetWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, HomeActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.pet_widget);
+            views.setOnClickPendingIntent(R.id.pet_widget_layout, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app
+            // widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 }
