dafa5b600513cdebf832ea8cf8cf5433ab855c2f
Liam Xue <liamxue2002@gmail.com>
2021-03-27 21:55:37 -0600

    update notification

--- app/src/main/java/com/example/pet/MyService.java
+++ app/src/main/java/com/example/pet/MyService.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 
 import androidx.core.app.NotificationCompat;
@@ -30,21 +31,30 @@ public class MyService extends Service {
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
+                User user = User.Initialize();
+                String str = null;
+                try {
+                    str = user.pet.getPhrase();
+                } catch (Exception e) {
+                    Log.i("home", e.toString());
+                }
+                if (str != null) {
+                    Notification notification = new NotificationCompat.Builder(context, NotificationApplication.CHANNEL_ID)
+                            .setSmallIcon(R.drawable.snail_t)
+                            .setContentTitle(user.getPetName()+": ")
+                            .setContentText(str)
+                            .setTicker(str)
+                            .setPriority(NotificationCompat.PRIORITY_MAX)
+                            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                            .setAutoCancel(true)
+                            .build();
 
-                Notification notification = new NotificationCompat.Builder(context, NotificationApplication.CHANNEL_ID)
-                        .setSmallIcon(R.drawable.snail_t)
-                        .setContentTitle("Pet")
-                        .setContentText("Hallo!")
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                        .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                        .build();
-
-                notificationManager.notify(1, notification);
-
-                refreshHandler.postDelayed(this, 120 * 1000);
+                    notificationManager.notify(1, notification);
+                }
+                refreshHandler.postDelayed(this, 10 * 1000);
             }
         };
-        refreshHandler.postDelayed(runnable, 120 * 1000);
+        refreshHandler.postDelayed(runnable, 10 * 1000);
 
         return START_STICKY;
     }
