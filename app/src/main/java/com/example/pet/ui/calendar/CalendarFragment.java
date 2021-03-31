100d6aa3da06d1a1d07b6299a7c636f35dfe561b
Liam Xue <liamxue2002@gmail.com>
2021-03-31 17:14:18 -0600

    Update CalendarFragment.java

--- app/src/main/java/com/example/pet/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/pet/ui/calendar/CalendarFragment.java
@@ -53,12 +53,10 @@ public class CalendarFragment extends Fragment {
     private TextView top_date_text;
     private TextView[] table_text;
     private LinearLayout[] table_text_frame;
+    private LinearLayout[] table_text_task;
 
     private int[] IMAGE_SOURCE;
     private int[] TASK_ROUND_ICON;
-
-    private final int TABLE_X_MAX = 140;
-    private final int TABLE_Y_MAX = 160;
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -70,7 +68,7 @@ public class CalendarFragment extends Fragment {
 
         user = User.Initialize();
         IMAGE_SOURCE = new int[]{R.drawable.ic_baseline_education_24, R.drawable.ic_baseline_habbit_24, R.drawable.ic_baseline_sport_24, R.drawable.ic_baseline_work_24};
-        TASK_ROUND_ICON = new int[]{R.drawable.background_round_color_education, R.drawable.background_round_color_sport, R.drawable.background_round_color_habbit, R.drawable.background_round_color_work};
+        TASK_ROUND_ICON = new int[]{R.drawable.background_round_color_education, R.drawable.background_round_color_habbit, R.drawable.background_round_color_sport, R.drawable.background_round_color_work};
 
         currentYear = Integer.parseInt(new SimpleDateFormat("yyyy", Locale.getDefault()).format(new Date()));
         currentMonth = Integer.parseInt(new SimpleDateFormat("M", Locale.getDefault()).format(new Date()));
@@ -116,6 +114,7 @@ public class CalendarFragment extends Fragment {
 
         table_text = new TextView[35];
         table_text_frame = new LinearLayout[35];
+        table_text_task = new LinearLayout[35];
         for (int _i = 0; _i < 5; _i++) {
             for (int _j = 0; _j < 7; _j++) {
                 int i = _i;
@@ -128,13 +127,23 @@ public class CalendarFragment extends Fragment {
                         updateDisplayDate(i, j);
                     }
                 });
+                table_text_task[i * 7 + j] = root.findViewById(r.getIdentifier("calendar_table_task_" + i + j, "id", getContext().getPackageName()));
             }
         }
 
         updateTable();
         updateBottomScrollView();
-
+        int[] c = {0, 1, 2, 3};
         return root;
+    }
+
+    private void showDots(LinearLayout parentLayout, int[] colors) {
+        for (int color : colors) {
+            TextView tv = new TextView(getContext());
+            tv.setBackground(getResources().getDrawable(TASK_ROUND_ICON[color]));
+            tv.setText("  ");
+            parentLayout.addView(tv);
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -153,6 +162,7 @@ public class CalendarFragment extends Fragment {
         for (; indx < start_day; indx++) {
             table_text_frame[indx].setBackgroundColor(0x00000000);
             table_text[indx].setText("");
+            table_text_task[indx].removeAllViews();
         }
         int dayNum = 1;
         for (; indx < start_day + YearMonth.of(displayYear, displayMonth).lengthOfMonth() && indx < 35; indx++) {
@@ -167,11 +177,21 @@ public class CalendarFragment extends Fragment {
                 table_text_frame[indx].setBackground(drawable);
             }
             table_text[indx].setText("" + dayNum);
+
+            table_text_task[indx].removeAllViews();
+            ArrayList<Task> theseTasks = user.getTasks(displayYear, displayMonth, dayNum);
+            int[] color = new int[Math.min(theseTasks.size(), 4)];
+            for (int i = 0; i < Math.min(theseTasks.size(), 4); i++) {
+                color[i] = theseTasks.get(i).parentType();
+            }
+            showDots(table_text_task[indx], color);
+
             dayNum++;
         }
         for (; indx < 35; indx++) {
             table_text_frame[indx].setBackgroundColor(0x00000000);
             table_text[indx].setText("");
+            table_text_task[indx].removeAllViews();
         }
     }
 
@@ -201,7 +221,7 @@ public class CalendarFragment extends Fragment {
             makeTextView("You don't have anything for this date.", makeInnerLayout(taskLayout));
         }
         // bottom-right # of tasks display
-        TextView bottom_tasks_num =root.findViewById(R.id.calendar_bottom_tasks_num);
+        TextView bottom_tasks_num = root.findViewById(R.id.calendar_bottom_tasks_num);
         bottom_tasks_num.setText("+" + String.valueOf(WINDOW_NUMBER) + " " + "tasks");
     }
 
@@ -211,35 +231,7 @@ public class CalendarFragment extends Fragment {
             displayDate = Integer.parseInt((String) table_text[i * 7 + j].getText());
         }
         updateBottomScrollView();
-
-        int start_day = 0;
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", java.util.Locale.ENGLISH);
-            Date myDate = sdf.parse("01/" + displayMonth + "/" + displayYear);
-            start_day = myDate.getDay();
-        } catch (Exception e) {
-            start_day = 0;
-        }
-        int indx = 0;
-        for (; indx < start_day; indx++) {
-            table_text_frame[indx].setBackgroundColor(0x00000000);
-        }
-        int dayNum = 1;
-        for (; indx < start_day + YearMonth.of(displayYear, displayMonth).lengthOfMonth() && indx < 35; indx++) {
-            table_text_frame[indx].setBackgroundColor(0x00000000);
-            if (displayYear == currentYear && displayMonth == currentMonth && dayNum == currentDate) {
-                Drawable drawable = getResources().getDrawable(R.drawable.calendar_date);
-                table_text_frame[indx].setBackground(drawable);
-            }
-            if (dayNum == displayDate) {
-                Drawable drawable = getResources().getDrawable(R.drawable.calendar_date_2);
-                table_text_frame[indx].setBackground(drawable);
-            }
-            dayNum++;
-        }
-        for (; indx < 35; indx++) {
-            table_text_frame[indx].setBackgroundColor(0x00000000);
-        }
+        updateTable();
     }
 
     private LinearLayout makeInnerLayout(LinearLayout parent) {
@@ -306,7 +298,7 @@ public class CalendarFragment extends Fragment {
         // DEADLINE
         TextView ddl = new TextView(getContext());
         ddl.setText(com.example.pet.Date.makeDateString(t.Year(), t.Month(), t.Day()));
-            ddl.setTextColor(getResources().getColor(R.color.grey));
+        ddl.setTextColor(getResources().getColor(R.color.grey));
         ddl.setTextSize(12);
         // params
         LinearLayout.LayoutParams paramsDDL = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1);
