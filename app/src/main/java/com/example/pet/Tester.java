77f263ba4bca61b964000ccb5f1ed466e8540807
Liam Xue <liamxue2002@gmail.com>
2021-03-31 18:17:10 -0600

    update custom user

--- app/src/main/java/com/example/pet/Tester.java
+++ app/src/main/java/com/example/pet/Tester.java
@@ -11,8 +11,16 @@ public class Tester {
         new File(path, "Tasks.json").delete();
     }
 
+    /*
+    goal type:
+    0 -- education
+    1 -- habbit
+    2 -- sport
+    3 -- work
+     */
+
     static public User makeCustomUser() {
-        final int today = 20210329;
+        final int today = 20210331;
 
         Tester.deleteAllJSON();
         User user = User.Initialize();
@@ -21,7 +29,7 @@ public class Tester {
         user.longTermGoal = "Graduate with laifu & waifu";
         user.longTermGoalStart = 20200901;
         user.longTermGoalEnd = 20240430;  // YYYYMMDD
-        user.addPet("Snail");
+        user.addPet("Bary");
 
         User.goalList = new ArrayList<>();
         user.nextGoalID = 0;
@@ -51,13 +59,6 @@ public class Tester {
 
 
         Goal goal2 = user.addGoal("Lose Weight", "Reduce my body weight to 10kg", 2020, 9, 1, 2021, 8, 31, 2);
-        t = user.addTask("Jogging", "", 2020, 9, 1, 2021, 8, 31, "Daily", goal2);
-        t = t.parent;
-        for (int i = 0; i < t.finished.size(); i++) {
-            if (t.recurringDates.get(i) < today) {
-                t.finished.set(i, 1);
-            }
-        }
         t = user.addTask("Go to GYM", "", 2020, 11, 8, 2021, 4, 20, "Weekly", goal2);
         t = t.parent;
         for (int i = 0; i < t.finished.size(); i++) {
@@ -77,7 +78,9 @@ public class Tester {
                 t.finished.set(i, 1);
             }
         }
-        t = user.addTask("Swimming", "", 2021, 3, 5, 2021, 8, 31, "Weekly", goal2);
+
+        Goal intern = user.addGoal("Become a Google Software Developer", "", 2020, 9, 30, 2021, 3, 31, 3);
+        t = user.addTask("Google Solution Challenge", "Work on the pet project.", 2021, 2, 3, "Daily", intern);
         t = t.parent;
         for (int i = 0; i < t.finished.size(); i++) {
             if (t.recurringDates.get(i) < today) {
@@ -85,23 +88,27 @@ public class Tester {
             }
         }
 
+        user.addTask("Solution Challenge Beta", "", 2021, 3, 27,"Once", intern);
+        user.addTask("Solution Challenge DDL", "", 2021, 3, 31, "Once", intern);
+
+        t = user.addTask("Write 3 question on LeetCode", "", 2021, 2, 2, "Weekly", intern);
+        t = t.parent;
+        for (int i = 0; i < t.finished.size(); i++) {
+            if (t.recurringDates.get(i) < today) {
+                t.finished.set(i, 1);
+            }
+        }
+
+
         Goal goal3 = user.addGoal("Survive First Year", "target: grade of 90", 2021, 1, 11, 2021, 4, 30, 0);
-        t = user.addTask("Reading", "", 2021, 1, 11, "Daily", goal3);
+        t = user.addTask("Reading", "", 2021, 1, 16, "Bi-weekly", goal3);
         t = t.parent;
         for (int i = 0; i < t.finished.size(); i++) {
             if (t.recurringDates.get(i) < today) {
                 t.finished.set(i, 1);
             }
         }
-        t = user.addTask("Google Solution Challenge", "Work on the pet project.", 2021, 2, 3, "Daily", goal3);
-        t = t.parent;
-        for (int i = 0; i < t.finished.size(); i++) {
-            if (t.recurringDates.get(i) < today) {
-                t.finished.set(i, 1);
-            }
-        }
-        user.addTask("Solution Challenge Beta", "", 2021, 3, 13,2021,3,28, "Weekly", goal3);
-        user.addTask("Solution Challenge DDL", "", 2021, 3, 31, "Once", goal3);
+
         t = user.addTask("Do Calc Assignment", "", 2021, 1, 16, "Weekly", goal3);
         t = t.parent;
         for (int i = 0; i < t.finished.size(); i++) {
@@ -131,6 +138,15 @@ public class Tester {
             }
         }
 
+        Goal goal4 = user.addGoal("Mental Health", " ", 2020, 9, 1, 2024, 4, 30, 1);
+        t = user.addTask("Movie night", "", 2020, 9, 4, "Weekly", goal4);
+        t = t.parent;
+        for (int i = 0; i < t.finished.size(); i++) {
+            if (t.recurringDates.get(i) < today) {
+                t.finished.set(i, 1);
+            }
+        }
+
         Goal goal = user.addGoal("Deal with next 3 years", " ", 2021, 9, 1, 2024, 4, 30, 1);
         user.addTask("Do things", "", 2021, 9, 1, "Daily", goal);
 
@@ -139,8 +155,20 @@ public class Tester {
         return user;
     }
 
-    static public User readCustomUser() {
+    static public User makeEmptyUser(){
+        Tester.deleteAllJSON();
         User user = User.Initialize();
+
+        user.isFirstTime = false;
+        user.longTermGoal = "goal";
+        user.longTermGoalStart = 20200901;
+        user.longTermGoalEnd = 20240430;  // YYYYMMDD
+        user.addPet("Snail");
+
+        User.goalList = new ArrayList<>();
+        user.nextGoalID = 0;
+        User.taskList = new ArrayList<>();
+
         return user;
     }
 }
